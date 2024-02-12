e9e58207e185c6e5750c4807042ddbf77b4a56c8
Cdiazargo83 <asistente.sistemas.snap21@gmail.com>
2024-02-12 11:01:20 -0500

    commit

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid', 100); // Limitar la longitud de la columna uuid
+            $table->string('uuid', 191); // Limitar la longitud de la columna uuid
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
