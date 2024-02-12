e44f043b5f44162a1dbdd86ad068d977a64b858b
Cdiazargo83 <asistente.sistemas.snap21@gmail.com>
2024-02-12 08:33:37 -0500

    actualiza

--- config/database.php
+++ config/database.php
@@ -44,6 +44,8 @@ return [
         ],
 
         'mysql' => [
+            
+
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -55,12 +57,21 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'prefix'    => '',
+                'strict' => true,
+                'engine' => 'InnoDB ROW_FORMAT=DYNAMIC', // Aquí puedes establecer el motor de almacenamiento y la configuración del índice
             ]) : [],
+
+
+
+
+            
         ],
 
         'pgsql' => [
